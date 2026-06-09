405c32cbfe29b3a55a53aa34b4d8c951a5c5bb09
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-09 08:23:20 +0200

    Develop console application for personal data entry and display in C++

---
+++ 003_User_Inputs/User_Inputs.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+// #include <cmath>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string Name;
+    cout << "Please Enter Your Name: ";
+    getline(cin, Name);
+
+    short Age;
+    cout << "Please Enetr Your Age: ";
+    cin >> Age;
+
+    string City;
+    cout << "Please Enter The City: ";
+    cin >> City;
+
+    string Country;
+    cout << "Please Enter The Country: ";
+    cin >> Country;
+
+    float MonthlySalary;
+    cout << "Please Enter Monthly Salary: ";
+    cin >> MonthlySalary;
+
+    float YearlySalary = MonthlySalary * 12;
+
+    char Gender;
+    cout << "Please Enter Gender: ";
+    cin >> Gender;
+
+    bool IsMarried;
+    cout << "You Are Married: ";
+    cin >> IsMarried;
+
+    cout << "\n\n******************************\n";
+    cout << "Name: " << Name << ". \n";
+    cout << "Age : " << Age << " Years. \n";
+    cout << "City: " << City << ". \n";
+    cout << "Country: " << Country << ". \n";
+    cout << "Monthly Salary: " << MonthlySalary << ". \n";
+    cout << "Yearly Salary: " << YearlySalary << ". \n";
+    cout << "Gender: " << Gender << ". \n";
+    cout << "Married: " << IsMarried << ". \n";
+    cout << "******************************\n\n";
+    return 0;
+}
