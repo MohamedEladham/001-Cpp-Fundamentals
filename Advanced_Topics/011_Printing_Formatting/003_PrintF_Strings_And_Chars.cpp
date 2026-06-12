6775594257a90d9ad8ea5b290d15f22e04f39a77
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-13 00:48:43 +0200

    Add Ather Another Example To PrintF_Strings_And_Chars Project

--- Advanced_Topics/011_Printing_Formatting/003_PrintF_Strings_And_Chars.cpp
+++ Advanced_Topics/011_Printing_Formatting/003_PrintF_Strings_And_Chars.cpp
@@ -46,5 +46,14 @@ int main()
 	printf("| %-15s | %-5d |\n", "Ahmed", 25);
 	printf("-------------------------------\n");
 
+	printf("\nStudent Information Using Setw\n");
+	cout << "---------|--------------------------------|---------|" << endl;
+	cout << "   Code  |               Name             |  Mark   |" << endl;
+	cout << "---------|--------------------------------|---------|" << endl;
+	cout << setw(9) << "C101" << "|" << setw(32) << "Introduction To Programming 1" << "|" << setw(9) << "95" << "|" << endl;
+	cout << setw(9) << "C102" << "|" << setw(32) << "Computer Hardware" << "|" << setw(9) << "88" << "|" << endl;
+	cout << setw(9) << "C10352" << "|" << setw(32) << "Network" << "|" << setw(9) << "75" << "|" << endl;
+	cout << "---------|--------------------------------|---------|" << endl;
+
 	return 0;
 }
