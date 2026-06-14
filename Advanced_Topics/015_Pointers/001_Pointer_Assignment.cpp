212afd69ff1ed5553b5133a63da395318153fd6b
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-14 08:02:58 +0200

    Update how pointers store and change memory addresses

--- Advanced_Topics/015_Pointers/001_Pointer_Assignment.cpp
+++ Advanced_Topics/015_Pointers/001_Pointer_Assignment.cpp
@@ -3,55 +3,58 @@ using namespace std;
 
 int main()
 {
-    // Create three integer variables and one pointer variable.
     int a = 10;
     int b = 20;
     int c = 30;
 
     int* ptr;
 
-    // Display the memory addresses of all variables.
     cout << "Address of a : " << &a << endl;
     cout << "Address of b : " << &b << endl;
     cout << "Address of c : " << &c << endl;
 
     cout << "\n---------------------\n";
 
-    // Assign the address of variable 'a' to the pointer.
+    
+    // Pointer points to a.
     ptr = &a;
 
-
-    // Result:
-    // ptr stores the address of 'a'.
-    // The address of ptr itself remains unchanged.
     cout << "ptr points to a\n";
+    
+    // ptr stores the address of 'a'.
     cout << "Stored Address : " << ptr << endl;
+    
+    // The address of ptr itself remains unchanged.
     cout << "Pointer Address: " << &ptr << endl;
 
     cout << "\n---------------------\n";
 
+
+    
     // Reassign the pointer to store the address of variable 'b'.
     ptr = &b;
-
-
-    // Result:
-    // ptr now stores the address of 'b'.
-    // The address of ptr itself is still unchanged.
+    
     cout << "ptr points to b\n";
+    
+    // ptr now stores the address of 'b'.
     cout << "Stored Address : " << ptr << endl;
+    
+    // The address of ptr itself is still unchanged.
     cout << "Pointer Address: " << &ptr << endl;
-
+    
     cout << "\n---------------------\n";
 
+    
+    
     // Reassign the pointer to store the address of variable 'c'.
     ptr = &c;
-
-
-    // Result:
-    // ptr now stores the address of 'c'.
-    // The address of ptr itself remains the same.
+    
     cout << "ptr points to c\n";
+    
+    // ptr now stores the address of 'c'.
     cout << "Stored Address : " << ptr << endl;
+    
+    // The address of ptr itself remains the same.
     cout << "Pointer Address: " << &ptr << endl;
 
     return 0;
