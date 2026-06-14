6008ff52eb6f6ef3230bbd698c683d3971e5f513
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-14 08:17:27 +0200

    Update Introduce pointer dereferencing for reading and modifying values

--- Advanced_Topics/015_Pointers/002_Pointer_Dereferencing.cpp
+++ Advanced_Topics/015_Pointers/002_Pointer_Dereferencing.cpp
@@ -10,29 +10,33 @@ int main()
 
     ptr = &a;
 
-    cout << "Value of a using ptr : " << *ptr << endl;
+    cout << "Value of a using ptr : " << *ptr << endl;   // Result => 10
+
+    // The address stored in ptr = address of a.
     cout << "Address stored in ptr: " << ptr << endl;
 
     cout << "\n-------------------\n";
 
     *ptr = 25;
 
-    cout << "Updated a value      : " << a << endl;
-    cout << "Value using ptr      : " << *ptr << endl;
+    cout << "Updated a value      : " << a << endl;      // Result => 25
+    cout << "Value using ptr      : " << *ptr << endl;   // Result => 25
 
     cout << "\n-------------------\n";
 
     ptr = &b;
 
-    cout << "Value of b using ptr : " << *ptr << endl;
+    cout << "Value of b using ptr : " << *ptr << endl;   // Result => 15
+    
+    // The address stored in ptr = address of b.
     cout << "Address stored in ptr: " << ptr << endl;
 
     cout << "\n-------------------\n";
 
     *ptr = 17;
 
-    cout << "Updated b value      : " << b << endl;
-    cout << "Value using ptr      : " << *ptr << endl;
+    cout << "Updated b value      : " << b << endl;      // Result => 17
+    cout << "Value using ptr      : " << *ptr << endl;   // Result => 17
 
     return 0;
 }
